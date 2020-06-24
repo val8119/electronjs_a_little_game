31ffa5981e846ed960e4b0f3e15dcc3f144c7e59
val8119 <mikk.vahaste@gmail.com>
2020-06-25 00:03:45 +0300

    Removed icon

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ let mainWindow;
 app.on("ready", function () {
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 600,
-        icon: "assets/images/icon.png"
+        height: 600
     });
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, "index.html"),
